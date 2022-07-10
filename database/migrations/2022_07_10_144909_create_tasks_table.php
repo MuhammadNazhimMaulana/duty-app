ef89e8612045f03f3ab038f96b65b3cc5ee48369
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-07-10 21:52:43 +0700

    Preparing Migration

---
+++ database/migrations/2022_07_10_144909_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('admin_id');
+            $table->string('title');
+            $table->integer('total_collectorss')->default(0);
+            $table->longText('description');
+            $table->timestamp('expired_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
