a57039a7be754197a5fe67e141e38539f4df3257
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-03-19 11:48:25 +0700

    A bit small update

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\providers\RepositoryProvider::class,
+        App\Providers\RepositoryProvider::class,
 
     ],
 
